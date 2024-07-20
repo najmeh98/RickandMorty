fcc51e8535dec1ded9566893cdaa9f829fa4144f
najmeh98 <foroughi.najmeh98@gmail.com>
2024-07-20 18:18:37 +0330

    Add useCharacter

---
+++ src/hooks/useCharacter.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+export default function useCharacter(url, query) {
+  const [characters, setCharacters] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    async function fetchdata() {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`${url}=${query}`, { signal });
+        setCharacters(data.results.slice(0, 5));
+      } catch (error) {
+        // fetch => err.name == "AbortError"
+        // axios.isCancel()
+        if (!axios.isCancel) {
+          setCharacters([]);
+          toast.error(error.response.data.error);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchdata();
+    return () => {
+      controller.abort();
+    };
+  }, [query]);
+
+  return {
+    isloading,
+    characters,
+  };
+}
